de6659ca66d31e520ad2cfc6782b009d10f053cb
hugoramirez93 <121719045+hugoramirez93@users.noreply.github.com>
2023-09-16 05:59:48 -0500

    Add files via upload
    Coursera JS Basics

---
+++ Basics Javascript/java.js
@@ -0,0 +1,175 @@
+// var show = prompt("provide your name","");
+
+// function hello(name){
+//     console.log(`Hello ${name}, and welcome!`);
+//     // console.log("Hello, and welcome!");
+//     // console.log(upperString);
+// }
+
+// hello(show);
+
+// // hello("Hugo");
+
+
+// // hello();
+
+// var yourName = prompt('provide your name','');
+// function hello(name){
+//     var greeting;
+
+//     if (name == null || name == ''){
+//         greeting = ("You didn't give me a name");
+//     }
+//     else {
+//         greeting = (`Hello ${name}, and welcome to my creed!`);
+//     }
+
+//     return greeting;
+// }
+
+// var yourName = prompt('provide your name','');
+// var food = ["cheese","fruit", "chicken", "dessert", "candy"];
+
+// function hello(name){
+
+// }
+
+// function hospitality(name, gift){
+
+//     console.log(hello(name));
+//     for (var i = 0; i<gift.length; i++) {
+//         console.log(`${name}, would you like some ${gift[i]}?`);
+//     }
+// }
+
+// hospitality(yourName, food);
+
+
+
+
+
+
+
+// var yourName = prompt("Put your name here","");
+// var food = ["cheese", "fruit", "chicken", "dessert", "candy"];
+
+// function hello(name){
+
+// }
+
+// function hospitality(name, gift) {
+//     console.log(hello(name));
+//     for (var i = 0; i<gift.length;i++){
+//         console.log(`${name}, would you like some ${gift[i]}?`);
+//     }
+
+// }
+
+// hospitality(yourName, food);
+
+
+// var yourName = prompt("Put your name here","");
+// var food = ["cheese", "fruit", "chicken", "dessert", "candy"];
+
+// function hello(name){
+
+// }
+
+// function randomInt(min, max){
+//     return math.floor ((max - min + 1) * Math.random() ) + min;
+// }
+
+// function hospitality(name, gift){
+
+//     console.log(hello(name));
+//         var randomGift = gift[ randomInt ( 0, gift.length - 1)];
+//         console.log(`${name}, would you like some ${randomGift}?`);
+//     }
+
+//     hospitality(yourName, food);
+
+// var ColorsInletters = document.getElementById('one').style.color= "blue";
+
+// var Colors = document.getElementsByTagName('p');
+
+
+// for ( var i=0; i < Colors.length; i++){
+//     Colors[i].style.color = "red";
+//     alert(`showing paragraph ${i}`);
+// }
+
+
+// var myClass = document.getElementsByClassName('Two');
+
+// for ( var i= 0; i < myClass.length; i++){
+//     myClass[i].style.color = "gold";
+// }
+
+var btn = document.querySelector('button');
+
+function ouch(){
+    alert("I told you not to press me!");
+}
+
+btn.onclick = ouch;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// hello(yourName);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
